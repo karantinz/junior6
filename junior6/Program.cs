16c32f40740513f16bca26bc3c6a05e0dd059032
Zmeegolov <karantinz@mail.ru>
2023-03-29 20:24:53 +0300

    Добавьте файлы проекта.

---
+++ junior6/Program.cs
@@ -0,0 +1,18 @@
+﻿int gold;
+int crystal = 0;
+int crystalPrice = 10;
+
+Console.WriteLine("Сколько у вас золота");
+gold = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Наши кристалы стоят 1 к 10 золотых");
+Console.WriteLine("Сколько хотите купить");
+
+int crystalBuy = Convert.ToInt32(Console.ReadLine());
+
+gold = gold - crystalPrice * crystalBuy;
+
+crystal += crystalBuy;
+
+Console.WriteLine("Остаток золота: " + gold);
+Console.WriteLine("Кристаллы: " + crystal);
